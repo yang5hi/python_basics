3670fc58a1c5456f7e86d208ae06148a7eba9002
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-02-15 11:37:22 -0500

    First commit

---
+++ PyBank_YangShi/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 09:41:48 2021
+PyBank_YangShi
+@author: YangShi
+"""
+
---
+++ PyPoll_YangShi/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 09:42:44 2021
+PyPoll_YangShi
+@author: YangShi
+"""
+
