8725b46d3fafa0ee94d8f9c94fe8394c7ec4da0e
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-02-16 08:17:53 -0500

    Second commit

--- PyBank_YangShi/main.py
+++ PyBank_YangShi/main.py
@@ -1,7 +1,60 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Feb 13 09:41:48 2021
 PyBank_YangShi
 @author: YangShi
 """
+import os, csv
+
+budget_csv=os.path.join('r','..','Resources','budget_data.csv')
+
+month_count=0
+budget_sum=0
+budget_max=0    #max profit
+budget_min=0    #max loss
+output_path=os.path.join("r","..", "analysis","budget_analysis.txt")
+
+with open (budget_csv) as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+    csvheader=next(csvreader)       #find the header
+    for row in csvreader:
+        if float(row[1])>=budget_max:
+            budget_max=float(row[1])       #record greatest gain
+            month_max=row[0]        #record month with greatest gain
+        if float(row[1])<=budget_min:       
+            budget_min=float(row[1])       #record greatest loss
+            month_min=row[0]        #record month with greatest loss
+        month_count+=1              #find total number of months
+        budget_sum=budget_sum+float(row[1])        #find total profit/losses
+if month_count>0:
+    budget_average=round(float(budget_sum/month_count),2)     #cal the average
+
+#output to terminal
+    
+print("Financial Analysis")
+print("---------------------------------------")
+print(f"Total months:  {month_count}")
+print(f"Total $ {budget_sum}")
+print(f"Average Change: $  {budget_average}")
+print(f"Greatest Increase in Profits: $ {month_max} {budget_max}")
+print(f"Greasted Decrease in Profits: $ {month_min} {budget_min}")
+print("---------------------------------------")
+
+#output to text file
+with open (output_path, "w") as text:
+    text.write("Financial Analysis \n -------------------------------\n Total months: ")
+    text.write(str(month_count))
+    text.write("\n Total $" )
+    text.write(str(budget_sum))
+    text.write("\n Average Change: $" )
+    text.write(str(budget_average))
+    text.write("\n Greatest Increase in Profits: ")
+    text.write(month_max)
+    text.write("  ($")
+    text.write(str( budget_max))
+    text.write(") \n Greatest Decrease in Profits: ")
+    text.write(month_min)
+    text.write("  ($")
+    text.write(str( budget_min))
+    text.write(") \n -------------------------------")
+
 
--- PyPoll_YangShi/main.py
+++ PyPoll_YangShi/main.py
@@ -4,4 +4,59 @@ Created on Sat Feb 13 09:42:44 2021
 PyPoll_YangShi
 @author: YangShi
 """
+"""
+Created on Mon Feb 15 09:54:31 2021
+
+"""
+import os, csv
+
+#input file path
+election_csv=os.path.join('r','..','Resources','election_data.csv')
+
+votes=[]                #list of valid votes
+candidate_percent=[]    #percentage of votes each candidate won
+candidate_votes=[]      #total number of votes each candidate won
+
+output_path=os.path.join("r","..", "analysis","election_analysis.txt")
+
+with open (election_csv) as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+    csvheader=next(csvreader)       #find the header
+    for row in csvreader:
+        votes.append(row[2])
+total_votes=len(votes)              #total number of votes cast
+candidates=list(set(votes))
+
+Num_of_candidate=len(candidates)
+#output to the terminal
+print("Election Results")
+print("--------------------------------------------------")
+print(f"Total Votes:  {total_votes}")
+print("--------------------------------------------------")
+#calcuate the election results and put into lists
+for i in range(Num_of_candidate):
+    candidate_votes.append(votes.count(candidates[i]))
+    candidate_percent.append(round(candidate_votes[i]/total_votes*100,3))
+    print(f"{candidates[i]} : {candidate_percent[i]}%  ({candidate_votes[i]})")
+#find the winner
+winner=candidates[candidate_votes.index(max(candidate_votes))]
+print("--------------------------------------------------")
+print(f"Winner:  {winner}")
+print("--------------------------------------------------")
+
+#output to text file
+with open (output_path,"w",newline='') as text:
+    text.write("Election Results \n -------------------------------\n Total Votes: ")
+    text.write(str(total_votes))
+    text.write("\n ------------------------------- \n" )
+    for i in range(Num_of_candidate):
+        text.write(str(candidates[i]))
+        text.write(":  ")
+        text.write(str(candidate_percent[i]))
+        text.write("%   (")
+        text.write(str(candidate_votes[i]))
+        text.write(")\n")
+    text.write("------------------------------------\n Winner:   ")
+    text.write(str(winner))
+    text.write(" \n -----------------------------------------")
 
