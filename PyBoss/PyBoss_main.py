04892872fd7ab5039d30cbbd55909fd4066a6f42
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-02-16 09:47:31 -0500

    commit num04

---
+++ PyBoss/PyBoss_main.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 09:35:14 2021
+PyBoss
+@author: YangShi
+
+Your script will need to do the following:
+* Import the `employee_data.csv` file, which currently holds employee records like the below:
+```csv
+Emp ID,Name,DOB,SSN,State
+214,Sarah Simpson,1985-12-04,282-01-8166,Florida
+15,Samantha Lara,1993-09-08,848-80-7526,Colorado
+411,Stacy Charles,1957-12-20,658-75-8526,Pennsylvania
+```
+* Then convert and export the data to use the following format instead:
+
+```csv
+Emp ID,First Name,Last Name,DOB,SSN,State
+214,Sarah,Simpson,12/04/1985,***-**-8166,FL
+15,Samantha,Lara,09/08/1993,***-**-7526,CO
+411,Stacy,Charles,12/20/1957,***-**-8526,PA
+```
+
+* In summary, the required conversions are as follows:
+
+  * The `Name` column should be split into separate `First Name` and `Last Name` columns.
+
+  * The `DOB` data should be re-written into `MM/DD/YYYY` format.
+
+  * The `SSN` data should be re-written such that the first five numbers are hidden from view.
+
+  * The `State` data should be re-written as simple two-letter abbreviations.
+
+* Special Hint: You may find this link to be helpful—[Python Dictionary for State Abbreviations](https://gist.github.com/afhaque/29f0f4f37463c447770517a6c17d08f5).
+
+"""
+import os, csv
+
+employee_csv=os.path.join("r","..","employee_data.csv")
+output_csv=os.path.join("r","..","formatted_data.csv")
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+with open (employee_csv) as csvfile:
+    csvreader=csv.reader(csvfile, delimiter=",")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
