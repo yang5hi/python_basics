81c131918fd1bcc283890d66da54f762815d6cc0
Yang_Shi <puckerygirlyoung@hotmail.com>
2021-02-21 19:15:19 -0500

    GPP update

--- PyParagraph/pyparagraph_main.py
+++ PyParagraph/pyparagraph_main.py
@@ -8,16 +8,16 @@ import os,re
 txt_input1=os.path.join("r","..","raw_data","paragraph_1.txt")
 txt_input2=os.path.join("r","..","raw_data","paragraph_2.txt")
 
-def split_count (txt_input):     #function to analyse the paragraph
+def split_count (txt_input):                            #function to analyse the paragraph
     with open(txt_input,"r",encoding="utf-8") as text:  #open text file
-        paragraph_sc=text.read().replace("\n", " ")                   #read text content, replace \n with space
-        letter_count_list=[]
-        paragraph_sentence=re.split("(?<=[.!?]) +", paragraph_sc)   #split a paragragh to sentences
+        paragraph_sc=text.read().replace("\n", " ")                     #read text content, replace \n with space
+        letter_count_list=[]            
+        paragraph_sentence=re.split("(?<=[.!?]) +", paragraph_sc)       #split a paragragh to sentences
         sentence_count=len(paragraph_sentence)           #count sentence
-        paragraph_word=paragraph_sc.split(" ")       #count words in a paragraph
+        paragraph_word=paragraph_sc.split(" ")           #count words in a paragraph
         for i in range(len(paragraph_word)):
-                paragraph_letter=list(paragraph_word[i])    #split words into letters
-                letter_count_list.append(len(paragraph_letter))           #store the number of the letters into a list
+                paragraph_letter=list(paragraph_word[i])                #split words into letters
+                letter_count_list.append(len(paragraph_letter))         #store the number of the letters into a list
         word_count=len(paragraph_word)                   #find the word count
         average_letter_count=round(sum(letter_count_list)/len(letter_count_list),1)    #calculate the average letter count
         average_sentence_length=round(word_count/sentence_count,1)              #calculate the average sentence length
@@ -28,5 +28,5 @@ def split_count (txt_input):     #function to analyse the paragraph
         print(f"Average Letter Count: {average_letter_count}")              #output average letter count
         print(f"Average Sentence Length: {average_sentence_length}")        #output average sentence length
 
-split_count(txt_input1)
-split_count(txt_input2)
+split_count(txt_input1)     #run paragraph1 analysis with split_count function
+split_count(txt_input2)     #run paragraph2 analysis with split_count function
